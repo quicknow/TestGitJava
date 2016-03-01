a36d7e55fa0188b3f162e7060e248055327d4122
zhuwenbing <594191959@qq.com>
2016-03-01 16:49:47 +0800

    后退到前一版本，改了又提交，看是否和远程版本冲突以及解决办法

--- TestGit2/src/Tiger.java
+++ TestGit2/src/Tiger.java
@@ -4,6 +4,8 @@ public class Tiger {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I am a tiger,ga ga ga !");
+		
+		System.out.println("I add a tiger now, but not near the last one!");
 	}
 
 }
