f305933315a5ae227c257bd6bb5aecfeb158b5bd
zhuwenbing <594191959@qq.com>
2016-03-03 10:32:06 +0800

    git revert HEAD  testing

--- TestGit2/src/Tiger.java
+++ TestGit2/src/Tiger.java
@@ -5,7 +5,9 @@ public class Tiger {
 		// TODO Auto-generated method stub
 		System.out.println("I am a tiger,ga ga ga !");
 		
-		System.out.println("I add a tiger now, but not near the last one!");
+		
+		
+		System.out.println("git revert HEAD zhan tie");
 	}
 
 }
